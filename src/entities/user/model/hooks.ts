cf7e28e6dd72f2af2f41c2fb26f9f74c4043fdcf
Alex Lyadnik <lyadnik47@gmail.com>
2026-03-27 13:24:48 +0300

    fix

--- src/entities/user/model/hooks.ts
+++ src/entities/user/model/hooks.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+/* import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { userApi } from '../api/userApi';
 import { User } from './interfaces';
@@ -47,3 +47,4 @@ export const useUpdateProfile = () => {
 		},
 	});
 };
+ */
